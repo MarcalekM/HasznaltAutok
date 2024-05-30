03d326317a840be8a323c7aeacc1077dd23fa5e3
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2024-05-30 14:04:18 +0200

    Add project files.

---
+++ HasznaltAutok/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HasznaltAutok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Auto> autok;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            autok = new();
+            using StreamReader sr = new(
+                path: @"..\..\..\src\Autok.txt",
+                encoding: System.Text.Encoding.UTF8);
+            while (!sr.EndOfStream) autok.Add(new Auto(sr.ReadLine()));
+            ListedCars.ItemsSource = autok;
+        }
+
+        public void ListedCars_SelectionChange()
+        {
+
+        }
+    }
+}
---
+++ HasznaltAutok/auto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HasznaltAutok
+{
+    internal class Auto
+    {
+        private string marka;
+        private string tipus;
+        private string meghajtas;
+        private string uzemanyag;
+        private int ar;
+
+        public Auto(string sor)
+        {
+            var adatok = sor.Split(',').ToList();
+            marka = adatok[0];
+            tipus = adatok[1];
+            meghajtas = adatok[2];
+            uzemanyag= adatok[3];
+            ar = int.Parse(adatok[4]);
+        }
+    }
+
+}
